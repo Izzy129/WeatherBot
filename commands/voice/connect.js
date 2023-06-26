d7f102e873be522fa2e54b9d50cbd7f5e7e11c3a
BuildTools <unconfigured@null.spigotmc.org>
2023-06-26 01:32:56 -0500

    Updated Connect

--- commands/voice/connect.js
+++ commands/voice/connect.js
@@ -1,31 +1,23 @@
-const {SlashCommandBuilder, ChannelType} = require('discord.js');
+const {SlashCommandBuilder} = require('discord.js');
 const { joinVoiceChannel, getVoiceConnections } = require('@discordjs/voice');
-// const { } = require('discord.js');
-// const { join } = require('lodash');
 
 const data = new SlashCommandBuilder()
     .setName('connect')
-    .setDescription('Connects the bot to a voice channel')
-    .addChannelOption(option => option.setName('channel')
-        .setDescription('The voice channel to connect to')
-        .setRequired(true)
-        .addChannelTypes(ChannelType.GuildVoice));
+    .setDescription('Connects the bot to a voice channel');        
 module.exports = {
     data: data,
-    async execute(interaction) {
-        // add a check to see if bot is already in a voice channel
+    async execute(interaction) {        
         if (getVoiceConnections().has(interaction.guildId)) {
             await interaction.reply('Bot is already in a voice channel! \nPlease disconnect before requesting!');
             return;
         }
-        const voiceConnection = joinVoiceChannel({
-            channelId: interaction.options.getChannel('channel').id,
+        joinVoiceChannel({
+            channelId: interaction.member.voice.channelId,
             guildId: interaction.guildId,
             adapterCreator: interaction.guild.voiceAdapterCreator
         });
 
-        // console.log(getVoiceConnections());
-        await interaction.reply('Connected!');
+        await interaction.reply('Connected to <#' + interaction.member.voice.channelId + '>!');
         
     }
 };
