312ac2a6f1c4b20d0ad4b8354aff7f6263915956
BuildTools <unconfigured@null.spigotmc.org>
2023-06-27 18:24:35 -0500

    Added rock paper scissors command (/rps)

---
+++ commands/fun/rockpaperscissors.js
@@ -0,0 +1,57 @@
+const {SlashCommandBuilder, ButtonBuilder, ButtonStyle ,ActionRowBuilder} = require('discord.js');
+const {rps} = require('C:\\Users\\Izzy\\Desktop\\WeatherBot\\commands\\fun\\rpsLogic.js');
+const data = new SlashCommandBuilder()
+    .setName('rps')
+    .setDescription('Play rock paper scissors with the bot! 🪨 📄 ✂️');
+module.exports = {
+    data: data,
+    async execute(interaction) {
+
+		const rock = new ButtonBuilder( {
+            label: 'Rock',
+            style: ButtonStyle.Primary,
+            customId: 'rock',
+            emoji: '🪨'
+        })
+
+        const paper = new ButtonBuilder( {
+            label: 'Paper',
+            style: ButtonStyle.Primary,
+            customId: 'paper',
+            emoji: '📄'
+        })
+
+        const scissors = new ButtonBuilder( {
+            label: 'Scissors',
+            style: ButtonStyle.Primary,
+            customId: 'scissors',
+            emoji: '✂️'
+        })
+
+		const row = new ActionRowBuilder()
+			.addComponents(rock, paper, scissors);
+
+		const response = await interaction.reply({
+			content: `Rock, Paper, Scissors, Shoot!`,
+			components: [row],
+		});
+
+
+        // collect responses
+        const filter = i => i.user.id === interaction.user.id; // checks if the button press was from the command user
+        try {
+            const collector = await response.awaitMessageComponent({ filter: filter, time: 15000}) 
+            
+            if (collector.customId === 'rock') {
+                await collector.update({ content: 'You chose rock!\n' + rps(collector.customId), components: [] });
+            } else if (collector.customId === 'paper') {
+                await collector.update({ content: 'You chose paper!\n' + rps(collector.customId), components: [] });
+            } else if (collector.customId === 'scissors') {
+                await collector.update({ content: 'You chose scissors!\n' + rps(collector.customId), components: [] });
+            }
+        } catch (error) {
+            await interaction.editReply({ content: 'You took too long to respond!', components: [] }); // idk if this will do anything tbh
+        }
+
+    }
+};
