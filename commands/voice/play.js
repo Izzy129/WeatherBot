ac708a1afdaae82bd34bb19c97c73a88c78213d3
Izzy129 <>
2023-07-17 03:04:36 -0500

    added manual service selection to play

--- commands/voice/play.js
+++ commands/voice/play.js
@@ -1,18 +1,31 @@
 // todo: add service image (spotify logo etc)
 // add option to specific which service to search
-
+// fix yt short support
 const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 const {joinVoiceChannel, getVoiceConnections} = require('@discordjs/voice');
 const { useMainPlayer } = require('discord-player'); // useMainPlayer is a function that returns the main player instance
 
 const {colorError, colorSuccess} = require('../../config.json');
+const { YouTubeExtractor, SpotifyExtractor, AppleMusicExtractor, SoundCloudExtractor, VimeoExtractor, YoutubeExtractor } = require('@discord-player/extractor');
 const data = new SlashCommandBuilder()
     .setName('play')
     .setDescription('Play an online song! (YouTube, Spotify, SoundCloud, etc.) 🎶')
     .addStringOption(option =>
-        option.setName('link')
-            .setDescription('Link to the YouTube video or playlist to play (Live videos are not supported yet)')
+        option.setName('query')
+            .setDescription('Queue or link to song to play')
             .setRequired(true)
+    )
+    .addStringOption(option =>
+        option.setName('service')
+            .setDescription('Service to search (YouTube, Spotify, SoundCloud, etc.)')
+            .setRequired(true)
+            .addChoices(
+                {name: 'YouTube', value: 'youtube'},
+                {name: 'Spotify', value: 'spotify'},
+                {name: 'SoundCloud', value: 'soundcloud'},
+                {name: 'Apple Music', value: 'applemusic'},
+                {name: 'Vimeo', value: 'vimeo'}
+            )
     );
 
 module.exports = {
@@ -40,14 +53,39 @@ module.exports = {
         
 
         const player = useMainPlayer();
-        const query = interaction.options.getString('link');
-
+        const query = interaction.options.getString('query');
+        
+        let service = null;
+        switch (interaction.options.getString('service')) {
+            case 'youtube':
+                service = `ext:${YouTubeExtractor.identifier}`;
+                break;
+            case 'spotify':
+                service = `ext:${SpotifyExtractor.identifier}`;
+                break;
+            case 'soundcloud':
+                service = `ext:${SoundCloudExtractor.identifier}`;
+                break;
+            case 'applemusic':
+                service = `ext:${AppleMusicExtractor.identifier}`;
+                break;
+            case 'vimeo':
+                service = `ext:${VimeoExtractor.identifier}`;
+                break;
+            default:
+                service = `ext:${YouTubeExtractor.identifier}`;
+        }
         const searchResult = await player.search(query, {
-            requestedBy: interaction.user
+            requestedBy: interaction.user,
+            searchEngine: `ext:${YoutubeExtractor.identifier}`
         });
-        console.log(searchResult);
+        
         if (!searchResult || searchResult.tracks.length === 0) { // no result found
-            return await interaction.editReply('No results were found!');
+            const embed = new EmbedBuilder()
+                .setColor(colorError)
+                .setTitle('Error')
+                .setDescription('No results were found!');
+            await interaction.editReply({ embeds: [embed] });
         }
 
         if ( // checks if query is a youtube live video 
@@ -98,5 +136,5 @@ module.exports = {
             .setTitle('Added song to queue!')
             .setDescription(`${durationFormat} **[${track.title}](${track.url})**`);
         await interaction.editReply({ embeds: [embed] });
-        }
-};
+        }       
+}
